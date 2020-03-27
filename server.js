b945e78fd8f890971474e8a7149f9fcaf30ce217
yingjieweb <1184061078@qq.com>
2020-03-27 09:28:23 +0800

    加载分页数据

--- server.js
+++ server.js
@@ -1,56 +1,76 @@
-let http = require('http')
-let fs = require('fs')
-let url = require('url')
-let port = process.argv[2]
+let http = require('http');
+let fs = require('fs');
+let url = require('url');
+let port = process.argv[2];
 
 if(!port){
-  console.log('指定端口号不好吗？\nnode server.js 8888 这样不会吗？')
-  process.exit(1)
+  console.log('指定端口号不好吗？\nnode server.js 8888 这样不会吗？');
+  process.exit(1);
 }
 
 let server = http.createServer(function(request, response){
-  let parsedUrl = url.parse(request.url, true)
-  let pathWithQuery = request.url 
-  let queryString = ''
+  let parsedUrl = url.parse(request.url, true);
+  let pathWithQuery = request.url ;
+  let queryString = '';
   if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
-  let path = parsedUrl.pathname
-  let query = parsedUrl.query
-  let method = request.method
+  let path = parsedUrl.pathname;
+  let query = parsedUrl.query;
+  let method = request.method;
 
   /********************/
 
-  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery);
 
   if(path === '/'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(fs.readFileSync('src/index.html'))
-    response.end()
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html;charset=utf-8');
+    let html = fs.readFileSync('src/index.html').toString();
+    let page = fs.readFileSync('database/page1.json').toString();
+    const array = JSON.parse(page);
+    const result = array.map(item=>`<li>${item.id}</li>`).join('');
+    html = html.replace('{{page}}',`<ul id="ul">${result}</ul>`);
+    response.write(html);
+    response.end();
   } else if(path === '/style.css'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(fs.readFileSync('src/style.css'))
-    response.end()
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/css;charset=utf-8');
+    response.write(fs.readFileSync('src/style.css'));
+    response.end();
   } else if(path === '/main.js'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
-    response.write(fs.readFileSync('src/main.js'))
-    response.end()
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/javascript;charset=utf-8');
+    response.write(fs.readFileSync('src/main.js'));
+    response.end();
   } else if(path === '/block.html'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(fs.readFileSync('src/block.html'))
-    response.end()
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html;charset=utf-8');
+    response.write(fs.readFileSync('src/block.html'));
+    response.end();
   } else if(path === '/message.xml'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/xml;charset=utf-8')
-    response.write(fs.readFileSync('src/message.xml'))
-    response.end()
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/xml;charset=utf-8');
+    response.write(fs.readFileSync('src/message.xml'));
+    response.end();
+  } else if(path === '/data.json'){
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/json;charset=utf-8');
+    response.write(fs.readFileSync('src/data.json'));
+    response.end();
+  } else if(path === '/page2.json'){
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/json;charset=utf-8');
+    response.write(fs.readFileSync('database/page2.json'));
+    response.end();
+  } else if(path === '/page3.json'){
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/json;charset=utf-8');
+    response.write(fs.readFileSync('database/page3.json'));
+    response.end();
   } else {
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
-    response.end()
+    response.statusCode = 404;
+    response.setHeader('Content-Type', 'text/html;charset=utf-8');
+    response.write(`你输入的路径不存在对应的内容`);
+    response.end();
   }
 
   /********************/
--- src/main.js
+++ src/main.js
@@ -20,13 +20,16 @@ getCss.onclick = ()=>{
 getJS.onclick = ()=>{
   let http = new XMLHttpRequest();
   http.open('GET','/main.js');
-  http.onload = ()=>{
-    let script = document.createElement('script');
-    script.innerHTML = http.response;
-    document.body.appendChild(script);
-  }
-  http.onerror = ()=>{
-    console.log('请求失败');
+  http.onreadystatechange = ()=>{
+    if (http.readyState === 4){ //当前仅表示下载完成，不知道是下载成功的完成还是失败的完成
+      if(http.status === 200){
+        let script = document.createElement('script');
+        script.innerHTML = http.response;
+        document.body.appendChild(script);
+      }else{
+        console.log('css加载失败')
+      }
+    }
   }
   http.send();
 }
@@ -34,13 +37,12 @@ getJS.onclick = ()=>{
 getHTML.onclick = ()=>{
   let http = new XMLHttpRequest();
   http.open('GET','/block.html');
-  http.onload = ()=>{
-    let template = document.createElement('div');
-    template.innerHTML = http.response;
-    document.body.appendChild(template);
-  }
-  http.onerror = ()=>{
-    console.log('请求失败');
+  http.onreadystatechange = ()=>{
+    if (http.readyState === 4) { //当前仅表示下载完成，不知道是下载成功的完成还是失败的完成
+      let template = document.createElement('div');
+      template.innerHTML = http.response;
+      document.body.appendChild(template);
+    }
   }
   http.send();
 }
@@ -62,3 +64,39 @@ getXML.onclick = ()=>{
   http.send();
 }
 
+getJSON.onclick = ()=>{
+  let http = new XMLHttpRequest();
+  http.open('GET','/data.json');
+  http.onreadystatechange = () => {
+    if (http.readyState === 4){ //当前仅表示下载完成，不知道是下载成功的完成还是失败的完成
+      if(http.status === 200){
+        span.textContent = JSON.parse(http.response).name;  //将json字符串转换为对象（不一定全是对象）
+      }else{
+        console.log('css加载失败')
+      }
+    }
+  }
+  http.send();
+}
+
+let pageFlag = 1;
+getPage.onclick = ()=>{
+  let http = new XMLHttpRequest();
+  http.open('GET',`/page${pageFlag + 1}.json`);
+  http.onreadystatechange = () => {
+    if (http.readyState === 4){ //当前仅表示下载完成，不知道是下载成功的完成还是失败的完成
+      if(http.status === 200){
+        let array = JSON.parse(http.response);
+        array.forEach(item => {
+          const li = document.createElement("li");
+          li.textContent = item.id;
+          ul.appendChild(li);
+        });
+        pageFlag += 1;
+      }else{
+        console.log('css加载失败')
+      }
+    }
+  }
+  http.send();
+}
