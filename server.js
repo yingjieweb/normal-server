1cf3aaefda456aac66cdc74c6adc96b963fa66c5
yingjieweb <1184061078@qq.com>
2020-03-27 08:20:28 +0800

    server

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
-var port = process.argv[2]
+let http = require('http')
+let fs = require('fs')
+let url = require('url')
+let port = process.argv[2]
 
 if(!port){
   console.log('指定端口号不好吗？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
-  var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
-  var queryString = ''
+let server = http.createServer(function(request, response){
+  let parsedUrl = url.parse(request.url, true)
+  let pathWithQuery = request.url 
+  let queryString = ''
   if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
-  var path = parsedUrl.pathname
-  var query = parsedUrl.query
-  var method = request.method
+  let path = parsedUrl.pathname
+  let query = parsedUrl.query
+  let method = request.method
 
   /********************/
 
@@ -24,15 +24,27 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`
-      <link rel="stylesheet" href="/style">
-      <h1> hello erha</h1>
-    `)
+    response.write(fs.readFileSync('src/index.html'))
     response.end()
-  } else if(path === '/style'){
+  } else if(path === '/style.css'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`h1{color: red;}`)
+    response.write(fs.readFileSync('src/style.css'))
+    response.end()
+  } else if(path === '/main.js'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+    response.write(fs.readFileSync('src/main.js'))
+    response.end()
+  } else if(path === '/block.html'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(fs.readFileSync('src/block.html'))
+    response.end()
+  } else if(path === '/message.xml'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/xml;charset=utf-8')
+    response.write(fs.readFileSync('src/message.xml'))
     response.end()
   } else {
     response.statusCode = 404
